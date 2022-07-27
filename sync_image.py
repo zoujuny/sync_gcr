e004f504916e2db4e90bdf8eae2cf289f10980fd
kenwood chan <kenwoodchan@kenwooddeMacBook-Pro.local>
2022-07-27 11:08:31 +0800

    update sync

--- sync_image.py
+++ sync_image.py
@@ -16,12 +16,24 @@ def get_filename():
 def pull_image():
     name_list= get_filename()
     for name in name_list:
-        new_name = "kenwood/" + name.split("/")[-1]
-        cmd = "docker tag {0}   {1}".format(name, new_name)
-        subprocess.call("docker pull {}".format(name), shell=True)
-        subprocess.run(["docker", "tag", name, new_name])
-        subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
-        subprocess.call("docker push {}".format(new_name), shell=True)
+        if 'sha256' in name:
+            print(name)
+            sha256_name = name.split("@")
+            new_name = sha256_name[0].split("/")[-1]
+            tag = sha256_name[-1].split(":")[-1][0:6]
+            image = "kenwood/" + new_name + ":"+ tag
+            cmd = "docker tag {0}   {1}".format(name, image)
+            subprocess.call("docker pull {}".format(name), shell=True)
+            subprocess.run(["docker", "tag", name, new_name])
+            subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
+            subprocess.call("docker push {}".format(new_name), shell=True)
+        else:
+            new_name = "kenwood/" + name.split("/")[-1]
+            cmd = "docker tag {0}   {1}".format(name, new_name)
+            subprocess.call("docker pull {}".format(name), shell=True)
+            subprocess.run(["docker", "tag", name, new_name])
+            subprocess.call("docker login -u kenwood -p qwer1234", shell=True)
+            subprocess.call("docker push {}".format(new_name), shell=True)
         
 if __name__ == "__main__":
     pull_image()
